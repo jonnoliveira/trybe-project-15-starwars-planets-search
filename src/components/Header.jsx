a6f01fe7e8be29575672fe20b65ae8fd1e46f6c2
jonnoliveira <jonnutri@gmail.com>
2023-05-20 15:07:40 -0300

    Add: Mobila version header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 import { useContext } from 'react';
 import InputFilterContext from '../context/InputFilterContext';
+import search from '../assets/search.svg';
+
+import '../css/header.css';
 
 function Header() {
   const {
@@ -9,53 +12,64 @@ function Header() {
 
   return (
     <header>
-      <h1>Star Wars - Planets Search</h1>
-
-      <div>
-        <label htmlFor="name">
-          TEXTO:
-          <input
-            type="text"
-            name="name"
-            value={ text }
-            onChange={ handleChange }
-            data-testid="name-filter"
-          />
-        </label>
+      <div className="title-container">
+        <h1>Star Wars</h1>
       </div>
 
-      <div>
-        <select
-          name="column-filter"
-          onChange={ ({ target }) => (
-            setOptions({ ...options, column: target.value })) }
-          data-testid="column-filter"
-        >
-          {
-            optionsList.map((option) => (
-              <option
-                data-testid="column-options"
-                key={ option }
-                value={ option }
-              >
-                { option }
-              </option>
+      <div className="input-text-conatiner">
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={ text }
+          onChange={ handleChange }
+          data-testid="name-filter"
+          placeholder="Dagobah ..."
+        />
+        <img src={ search } alt="Search icon" />
+      </div>
 
-            ))
-          }
-        </select>
+      <div className="filters">
+        <label htmlFor="column-filter">
+          Coluna
+          <select
+            id="column-filter"
+            name="column-filter"
+            onChange={ ({ target }) => (
+              setOptions({ ...options, column: target.value })) }
+            data-testid="column-filter"
+          >
 
-        <select
-          name="comparison-filter"
-          onChange={ ({ target }) => (
-            setOptions({ ...options, comparison: target.value })) }
-          data-testid="comparison-filter"
-        >
-          <option value="maior que">maior que</option>
-          <option value="menor que">menor que</option>
-          <option value="igual a">igual a</option>
+            {
+              optionsList.map((option) => (
+                <option
+                  data-testid="column-options"
+                  key={ option }
+                  value={ option }
+                >
+                  { option }
+                </option>
 
-        </select>
+              ))
+            }
+          </select>
+        </label>
+
+        <label htmlFor="comparison-filter">
+          Operador
+          <select
+            name="comparison-filter"
+            id="comparison-filter"
+            onChange={ ({ target }) => (
+              setOptions({ ...options, comparison: target.value })) }
+            data-testid="comparison-filter"
+          >
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+
+          </select>
+        </label>
 
         <input
           type="number"
@@ -65,6 +79,7 @@ function Header() {
             setOptions({ ...options, value: target.value })) }
           data-testid="value-filter"
         />
+
         <button
           type="button"
           onClick={ filterByOptions }
@@ -82,46 +97,55 @@ function Header() {
           LIMPAR FILTROS
         </button>
 
-        <select
-          name="column-sort"
-          onChange={ ({ target }) => setCol(target.value) }
-          data-testid="column-sort"
-        >
-          {
-            optionsList.map((option) => (
-              <option
-                data-testid="column-options"
-                key={ option }
-                value={ option }
-              >
-                { option }
-              </option>
+        <label htmlFor="column-sort">
+          Ordenar
+          <select
+            name="column-sort"
+            id="column-sort"
+            onChange={ ({ target }) => setCol(target.value) }
+            data-testid="column-sort"
+          >
 
-            ))
-          }
-        </select>
-        <label htmlFor="ASC">
-          <input
-            type="radio"
-            name="RADIO"
-            id="ASC"
-            data-testid="column-sort-input-asc"
-            value="ASC"
-            onChange={ ({ target }) => setSor(target.value) }
-          />
-          ASCENDENTE
+            {
+              optionsList.map((option) => (
+                <option
+                  data-testid="column-options"
+                  key={ option }
+                  value={ option }
+                >
+                  { option }
+                </option>
+
+              ))
+            }
+          </select>
         </label>
-        <label htmlFor="DESC">
-          <input
-            type="radio"
-            name="RADIO"
-            id="DESC"
-            data-testid="column-sort-input-desc"
-            value="DESC"
-            onChange={ ({ target }) => setSor(target.value) }
-          />
-          DESCENDENTE
-        </label>
+
+        <div className="radio">
+          <label htmlFor="ASC">
+            ASCENDENTE
+            <input
+              type="radio"
+              name="RADIO"
+              id="ASC"
+              data-testid="column-sort-input-asc"
+              value="ASC"
+              onChange={ ({ target }) => setSor(target.value) }
+            />
+          </label>
+
+          <label htmlFor="DESC">
+            DESCENDENTE
+            <input
+              type="radio"
+              name="RADIO"
+              id="DESC"
+              data-testid="column-sort-input-desc"
+              value="DESC"
+              onChange={ ({ target }) => setSor(target.value) }
+            />
+          </label>
+        </div>
 
         <button
           type="button"
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from 'react';
 import InputFilterContext from '../context/InputFilterContext';
 import useFetch from '../hooks/useFetch';
 
+import '../css/table.css';
+
 function Table() {
   const { dataFiltered, loading } = useContext(InputFilterContext);
   const { data, error } = useFetch();
@@ -20,43 +22,45 @@ function Table() {
             <p>Loading...</p>
           )
           : (
-            <table>
-              <thead>
-                <tr>
+            <div className="table-container">
+              <table>
+                <thead>
+                  <tr>
+                    {
+                      Object.keys(data[0]).map((key) => (
+                        <th key={ key }>{ key }</th>
+                      ))
+                    }
+                  </tr>
+                </thead>
+                <tbody>
                   {
-                    Object.keys(data[0]).map((key) => (
-                      <th key={ key }>{ key }</th>
+                    dataFiltered.map((planets) => (
+                      <tr key={ planets.name }>
+                        <td
+                          data-testid="planet-name"
+                        >
+                          { planets.name }
+                        </td>
+                        <td>{ planets.rotation_period }</td>
+                        <td>{ planets.orbital_period }</td>
+                        <td>{ planets.rotation_period }</td>
+                        <td data-testid="planet-diameter">{ planets.diameter }</td>
+                        <td>{ planets.climate }</td>
+                        <td>{ planets.gravity }</td>
+                        <td>{ planets.terrain }</td>
+                        <td>{ planets.surface_water }</td>
+                        <td>{ planets.population }</td>
+                        <td>{ planets.films }</td>
+                        <td>{ planets.created }</td>
+                        <td>{ planets.edited }</td>
+                        <td>{ planets.url }</td>
+                      </tr>
                     ))
                   }
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  dataFiltered.map((planets) => (
-                    <tr key={ planets.name }>
-                      <td
-                        data-testid="planet-name"
-                      >
-                        { planets.name }
-                      </td>
-                      <td>{ planets.rotation_period }</td>
-                      <td>{ planets.orbital_period }</td>
-                      <td>{ planets.rotation_period }</td>
-                      <td data-testid="planet-diameter">{ planets.diameter }</td>
-                      <td>{ planets.climate }</td>
-                      <td>{ planets.gravity }</td>
-                      <td>{ planets.terrain }</td>
-                      <td>{ planets.surface_water }</td>
-                      <td>{ planets.population }</td>
-                      <td>{ planets.films }</td>
-                      <td>{ planets.created }</td>
-                      <td>{ planets.edited }</td>
-                      <td>{ planets.url }</td>
-                    </tr>
-                  ))
-                }
-              </tbody>
-            </table>
+                </tbody>
+              </table>
+            </div>
           )
       }
     </div>
